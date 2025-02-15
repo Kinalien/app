f874b12ef1d0559b58f933256949a02a2577c548
LexSwed <lexswed@gmail.com>
2025-02-15 18:59:18 +0100

    remove web-vet

--- packages/web-vet/src/server/const.ts
+++
@@ -1,5 +0,0 @@
-export const RETURN_URL_COOKIE = "_nouvet_login_return_url";
-
-export const USER_TIMEZONE_COOKIE = "_nouvet_user_timezone";
-
-export const SESSION_COOKIE = "_nouvet_user";
--- packages/web-vet/src/server/i18n/shared.ts
+++
@@ -1,8 +0,0 @@
-export const acceptedLocaleLanguageTag = [
-	"en-US",
-	"en-GB",
-	"es-ES",
-	"es-MX",
-] as const satisfies Array<Intl.Locale["language"]>;
-
-export type SupportedLocale = (typeof acceptedLocaleLanguageTag)[number];
