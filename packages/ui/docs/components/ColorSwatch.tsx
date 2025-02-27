aac1c25fc5ed36eefa35b901b8af69c9348982b7
LexSwed <lexswed@gmail.com>
2025-02-27 20:42:04 +0100

    finish colors

---
+++ packages/ui/docs/components/ColorSwatch.tsx
@@ -0,0 +1,42 @@
+import { Show, createSignal } from "solid-js";
+import { Text } from "~/text";
+import { tw } from "~/tw";
+import { mergeDefaultProps } from "~/utils";
+
+export function ColorSwatch(ownProps: {
+	class: string;
+	variant?: "row" | "column";
+}) {
+	const props = mergeDefaultProps(ownProps, { variant: "row" });
+	const [color, setColor] = createSignal("");
+	const token = props.class
+		.split(" ")
+		.find((c) => c.startsWith("bg-"))
+		?.replace("bg-", "");
+	return (
+		<div
+			class={tw(
+				"flex gap-4",
+				props.variant === "row" ? "flex-row items-center" : "w-full flex-col",
+			)}
+			ref={() => {
+				const tokenColor = getComputedStyle(document.documentElement).getPropertyValue(
+					`--nou-${token}`,
+				);
+				setColor(tokenColor);
+			}}
+		>
+			<div class={tw("size-16 rounded-md border border-outline", props.class)} />
+			<div>
+				<Text as="code" class="font-medium font-mono">
+					{token}
+				</Text>
+				<Show when={color()}>
+					<Text as="div" class="text-sm" tone="light">
+						{color()}
+					</Text>
+				</Show>
+			</div>
+		</div>
+	);
+}
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -33,7 +33,7 @@ const buttonVariants = tv({
 		tone: {
 			neutral: "",
 			primary: "",
-			secondary: "",
+			tertiary: "",
 			success: "",
 			destructive: "",
 		},
@@ -47,19 +47,19 @@ const buttonVariants = tv({
 	compoundVariants: [
 		{
 			variant: "accent",
-			class: "intent:filter-darker [--btn-bg:var(--color-primary)]",
+			class: "intent:filter-darker [--nou-btn-bg:var(--color-primary)]",
 		},
 		{
 			variant: "accent",
-			tone: "secondary",
+			tone: "tertiary",
 			class:
-				"intent:filter-darker bg-tertiary text-on-tertiary outline-tertiary [--btn-bg:var(--color-tertiary)]",
+				"intent:filter-darker bg-tertiary text-on-tertiary outline-tertiary [--nou-btn-bg:var(--color-tertiary)]",
 		},
 		{
 			variant: "accent",
 			tone: "destructive",
 			class:
-				"bg-error intent:bg-error/90 text-on-error outline-error [--btn-bg:var(--color-error)]",
+				"bg-error intent:bg-error/90 text-on-error outline-error [--nou-btn-bg:var(--color-error)]",
 		},
 		{
 			variant: "outline",
@@ -69,31 +69,31 @@ const buttonVariants = tv({
 			variant: "outline",
 			tone: "destructive",
 			class:
-				"border-error intent:bg-error-container/30 text-error outline-error [--btn-bg:var(--color-surface)]",
+				"border-error intent:bg-error-container/30 text-error outline-error [--nou-btn-bg:var(--color-surface)]",
 		},
 		{
 			variant: "tonal",
 			tone: "neutral",
 			class:
-				"bg-on-surface/5 intent:bg-on-surface/8 text-on-surface [--btn-bg:var(--color-surface)]",
+				"bg-on-surface/5 intent:bg-on-surface/8 text-on-surface [--nou-btn-bg:var(--color-surface)]",
 		},
 		{
 			variant: "tonal",
 			tone: "primary",
 			class:
-				"intent:filter-darker bg-primary-container text-on-primary-container [--btn-bg:var(--color-primary-container)]",
+				"intent:filter-darker bg-primary-container text-on-primary-container [--nou-btn-bg:var(--color-primary-container)]",
 		},
 		{
 			variant: "tonal",
-			tone: "secondary",
+			tone: "tertiary",
 			class:
-				"intent:filter-darker bg-tertiary-container text-on-tertiary-container [--btn-bg:var(--color-tertiary-container)]",
+				"intent:filter-darker bg-tertiary-container text-on-tertiary-container [--nou-btn-bg:var(--color-tertiary-container)]",
 		},
 		{
 			variant: "tonal",
 			tone: "destructive",
 			class:
-				"bg-error/5 text-error [--btn-bg:var(--color-error/0.05)] hover:bg-error/8 focus:bg-error/12",
+				"bg-error/5 text-error [--nou-btn-bg:var(--color-error/0.05)] hover:bg-error/8 focus:bg-error/12",
 		},
 		{
 			variant: "tonal",
@@ -104,18 +104,18 @@ const buttonVariants = tv({
 		{
 			variant: "ghost",
 			tone: "neutral",
-			class: "[--btn-bg:var(--color-surface)] hover:bg-on-surface/5 focus:bg-on-surface/8",
+			class: "[--nou-btn-bg:var(--color-surface)] hover:bg-on-surface/5 focus:bg-on-surface/8",
 		},
 		{
 			variant: "ghost",
 			tone: "primary",
 			class:
-				"text-primary outline-primary [--btn-bg:var(--color-surface)] hover:bg-primary/5 focus:bg-primary/8",
+				"text-primary outline-primary [--nou-btn-bg:var(--color-surface)] hover:bg-primary/5 focus:bg-primary/8",
 		},
 		{
 			variant: "ghost",
 			tone: "destructive",
-			class: "text-error [--btn-bg:var(--color-error/0.05)] hover:bg-error/5 focus:bg-error/8",
+			class: "text-error [--nou-btn-bg:var(--color-error/0.05)] hover:bg-error/5 focus:bg-error/8",
 		},
 		{
 			variant: "link",
@@ -184,7 +184,7 @@ const BaseComponent = <T extends ValidComponent>(ownProps: BaseProps<T>) => {
 		>
 			{props.children}
 			<Show when={local.pending}>
-				<div class="absolute inset-0 isolate flex cursor-default items-center justify-center rounded-[inherit] bg-[radial-gradient(circle_at_50%_50%,color-mix(in_lch,var(--btn-bg)_92%,transparent)_30%,transparent)]">
+				<div class="absolute inset-0 isolate flex cursor-default items-center justify-center rounded-[inherit] bg-[radial-gradient(circle_at_50%_50%,color-mix(in_lch,var(--nou-btn-bg)_92%,transparent)_30%,transparent)]">
 					<Spinner size={local.size} />
 				</div>
 			</Show>
