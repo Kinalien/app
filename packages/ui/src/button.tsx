472e9cab1106891c5c8cf67e974466394989159d
LexSwed <lexswed@gmail.com>
2025-02-27 21:59:33 +0100

    button documentation

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,21 +6,21 @@ import { type VariantProps, tv, tw } from "./tw";
 import { type Merge, mergeDefaultProps } from "./utils";
 
 const buttonVariants = tv({
-	base: "relative border-none [text-box:_trim-both_cap_alphabetic] inline-flex cursor-default select-none items-center justify-center gap-2 rounded-full font-medium text-sm outline-offset-4 transition focus-visible:outline-4 disabled:pointer-events-none disabled:opacity-50",
+	base: "relative [text-box:_trim-both_cap_alphabetic] inline-flex cursor-default select-none items-center justify-center gap-2 rounded-full font-medium text-sm outline-offset-2 transition focus-visible:outline-2 disabled:pointer-events-none disabled:opacity-50",
 	variants: {
 		size: {
-			base: "min-h-12 min-w-12 px-5 py-2 text-base",
+			base: "min-h-12 min-w-12 px-5 py-2 text-sm",
 			sm: "min-h-10 min-w-10 px-3 text-sm",
 			lg: "min-h-16 min-w-16 px-8 text-lg sm:min-h-14 sm:min-w-14",
 			cta: "min-h-16 min-w-16 rounded-full px-8 text-lg",
 		},
 		variant: {
-			accent: "bg-primary text-on-primary outline-primary",
-			tonal: "outline-on-surface",
+			accent: "bg-primary border-none text-on-primary outline-primary",
+			tonal: "border-none outline-on-surface",
 			outline:
 				"border border-outline bg-transparent text-on-surface outline-on-surface outline-offset-0",
-			ghost: "text-on-surface outline-on-surface",
-			link: "text-primary underline-offset-4",
+			ghost: "border-none text-on-surface outline-on-surface",
+			text: "p-0 border-none text-primary underline-offset-4",
 		},
 		pending: {
 			true: "disabled:opacity-90",
@@ -34,7 +34,6 @@ const buttonVariants = tv({
 			neutral: "",
 			primary: "",
 			tertiary: "",
-			success: "",
 			destructive: "",
 		},
 	},
@@ -47,13 +46,14 @@ const buttonVariants = tv({
 	compoundVariants: [
 		{
 			variant: "accent",
-			class: "intent:filter-darker [--nou-btn-bg:var(--color-primary)]",
+			class:
+				"intent:bg-[color-mix(in_hsl,var(--nou-btn-bg)_92%,var(--nou-on-background))] [--nou-btn-bg:var(--color-primary)]",
 		},
 		{
 			variant: "accent",
 			tone: "tertiary",
 			class:
-				"intent:filter-darker bg-tertiary text-on-tertiary outline-tertiary [--nou-btn-bg:var(--color-tertiary)]",
+				"intent:bg-[color-mix(in_hsl,var(--nou-btn-bg)_92%,var(--nou-on-background))] bg-tertiary text-on-tertiary outline-tertiary [--nou-btn-bg:var(--color-tertiary)]",
 		},
 		{
 			variant: "accent",
@@ -63,7 +63,7 @@ const buttonVariants = tv({
 		},
 		{
 			variant: "outline",
-			class: "intent:bg-on-surface/8",
+			class: "intent:bg-on-surface/8 [--nou-btn-bg:var(--color-surface)]",
 		},
 		{
 			variant: "outline",
@@ -81,25 +81,19 @@ const buttonVariants = tv({
 			variant: "tonal",
 			tone: "primary",
 			class:
-				"intent:filter-darker bg-primary-container text-on-primary-container [--nou-btn-bg:var(--color-primary-container)]",
+				"intent:bg-[color-mix(in_hsl,var(--nou-btn-bg)_92%,var(--nou-on-background))] bg-primary-container text-on-primary-container [--nou-btn-bg:var(--color-primary-container)]",
 		},
 		{
 			variant: "tonal",
 			tone: "tertiary",
 			class:
-				"intent:filter-darker bg-tertiary-container text-on-tertiary-container [--nou-btn-bg:var(--color-tertiary-container)]",
+				"intent:bg-[color-mix(in_hsl,var(--nou-btn-bg)_92%,var(--nou-on-background))] bg-tertiary-container text-on-tertiary-container [--nou-btn-bg:var(--color-tertiary-container)]",
 		},
 		{
 			variant: "tonal",
 			tone: "destructive",
 			class:
-				"bg-error/5 text-error [--nou-btn-bg:var(--color-error/0.05)] hover:bg-error/8 focus:bg-error/12",
-		},
-		{
-			variant: "tonal",
-			tone: "success",
-			// TODO
-			class: "",
+				"bg-error/5 text-error [--nou-btn-bg:color-mix(in_oklab,var(--nou-error)_8%,var(--nou-background))] hover:bg-error/8 focus:bg-error/12",
 		},
 		{
 			variant: "ghost",
@@ -108,17 +102,12 @@ const buttonVariants = tv({
 		},
 		{
 			variant: "ghost",
-			tone: "primary",
-			class:
-				"text-primary outline-primary [--nou-btn-bg:var(--color-surface)] hover:bg-primary/5 focus:bg-primary/8",
-		},
-		{
-			variant: "ghost",
 			tone: "destructive",
-			class: "text-error [--nou-btn-bg:var(--color-error/0.05)] hover:bg-error/5 focus:bg-error/8",
+			class:
+				"text-error [--nou-btn-bg:color-mix(in_oklab,var(--nou-error)_8%,var(--nou-background))] hover:bg-error/5 focus:bg-error/8",
 		},
 		{
-			variant: "link",
+			variant: "text",
 			class: "intent:underline",
 		},
 	],
@@ -184,7 +173,7 @@ const BaseComponent = <T extends ValidComponent>(ownProps: BaseProps<T>) => {
 		>
 			{props.children}
 			<Show when={local.pending}>
-				<div class="absolute inset-0 isolate flex cursor-default items-center justify-center rounded-[inherit] bg-[radial-gradient(circle_at_50%_50%,color-mix(in_lch,var(--nou-btn-bg)_92%,transparent)_30%,transparent)]">
+				<div class="absolute inset-0 isolate flex cursor-default items-center justify-center rounded-[inherit] bg-[radial-gradient(circle_at_50%_50%,color-mix(in_lch,var(--nou-btn-bg)_98%,transparent)_30%,transparent)]">
 					<Spinner size={local.size} />
 				</div>
 			</Show>
